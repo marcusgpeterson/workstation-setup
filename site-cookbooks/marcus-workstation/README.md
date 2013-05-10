f3e1721ae4252c90768471ac50b8b2dcc5219585
Marcus Peterson <marcus.g.peterson@gmail.com>
2013-05-10 18:48:12 -0500

    Setup to start creating my own recipes.

---
+++ site-cookbooks/marcus-workstation/README.md
@@ -0,0 +1,3 @@
+marcus-workstation
+===============
+Recipes to set up Marcus's development machine.
---
+++ site-cookbooks/marcus-workstation/metadata.rb
@@ -0,0 +1,9 @@
+name             'marcus-workstation'
+maintainer       'Marcus Peterson'
+maintainer_email 'marcus.g.peterson@gmail.com'
+license          'MIT'
+description      'Installs/Configures marcus-workstation'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+supports         'mac_os_x'
+depends          'dmg'
