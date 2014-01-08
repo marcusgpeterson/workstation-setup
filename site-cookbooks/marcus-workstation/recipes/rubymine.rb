24e41cd719d4e3805b54158dfe71bc468b4bfc96
Marcus Peterson <marcus@x-134-84-251-177.uofm-secure.wireless.umn.edu>
2014-01-08 08:51:48 -0600

    Fixed RubyMine recipe.

--- site-cookbooks/marcus-workstation/recipes/rubymine.rb
+++ site-cookbooks/marcus-workstation/recipes/rubymine.rb
@@ -1,6 +1,6 @@
 dmg_package "RubyMine" do
-  volumes_dir "RubyMine-6.0.2"
-  source "http://www.jetbrains.com/ruby/download/"
+  volumes_dir "RubyMine"
+  source "http://download.jetbrains.com/ruby/RubyMine-6.0.2.dmg"
   checksum "b4ea1f3b7f5b7e70135a7c2f54a12cb5bc61776bbde7cbf6bbe3dd6a233f7b15"
   action :install
   owner node['current_user']
