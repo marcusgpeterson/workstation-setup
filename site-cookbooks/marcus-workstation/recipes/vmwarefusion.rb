9650b69c797bc09149c84e2226d36c3b0015d7a0
Marcus Peterson <marcusgpeterson@gmail.com>
2015-03-21 17:35:17 -0500

    Update vmwarefusion.rb

--- site-cookbooks/marcus-workstation/recipes/vmwarefusion.rb
+++ site-cookbooks/marcus-workstation/recipes/vmwarefusion.rb
@@ -1,7 +1,1 @@
-dmg_package "VMware Fusion" do
-  volumes_dir "VMware Fusion/"
-  source "http://download2us.softpedia.com/dl/5728857913226b6f31d2ae31ec4dbb2e/5197a89b/400024122/mac/System-Utilities/VMware-Fusion-5.0.3-1040386-light.dmg"
-  checksum "540355352cac92bf52aef1db393da75e573f3ad036d9edc51df3f7f2e140c0dd"
-  action :install
-  owner node['current_user']
-end
+homebrew_cask 'vmware-fusion'
